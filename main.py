e59415d357b2c541a58455c239e1e09e862b2970
arkhat-kaplan <artemskharlamov@mail.ru>
2021-10-19 04:42:55 +0300

    make it better

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def add_game(msg):
                        (msg.text, msg.from_user.id))
         con.commit()
     msg = bot.send_message(msg.from_user.id, text="Напиши дату игры")
-    bot.register_next_step_handler(msg, add_gamedate)
+    bot.register_next_step_handler(msg.msg, add_gamedate)
 
 
 def add_gamedate(msg):
